5b810e96a3954c0dd0f623c88594cc49dab62763
nugentmichael <nugent.michael@gmail.com>
2019-05-22 11:16:26 -0400

    Create dynamic table objects from data array.

--- App.js
+++ App.js
@@ -1,11 +1,40 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            data: [
+                {
+                    "id": 1,
+                    "name": "Foo",
+                    "age": 20
+                },
+                {
+                    "id": 2,
+                    "name": "Bar",
+                    "age": 30
+                },
+                {
+                    "id": 3,
+                    "name": "Baz",
+                    "age": 40
+                }
+            ]
+        };
+    }
+
     render() {
         return (
             <div>
                 <Header />
-                <Content />
+
+                <table>
+                    <tbody>
+                        {this.state.data.map((person, i) => <TableRow key={i} data={person} />)}
+                    </tbody>
+                </table>
             </div>
         )
     }
@@ -21,13 +50,14 @@ class Header extends Component {
     }
 }
 
-class Content extends Component {
+class TableRow extends Component {
     render() {
         return (
-            <div>
-                <h2>Content</h2>
-                <p>This is the content.</p>
-            </div>
+            <tr>
+                <td>{this.props.data.id}</td>
+                <td>{this.props.data.name}</td>
+                <td>{this.props.data.age}</td>
+            </tr>
         )
     }
 }
