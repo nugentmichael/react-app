c7ef56fc41fed73796f3b4324bfde527cd73d6f1
nugentmichael <nugent.michael@gmail.com>
2019-05-22 13:11:04 -0400

    Update the state of a component using an onClick event handler.

--- App.js
+++ App.js
@@ -1,43 +1,31 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            data: []
+        }
+
+        this.setStateHandler = this.setStateHandler.bind(this);
+    };
+
+    setStateHandler() {
+        const item = "setState...";
+        const myArray = this.state.data.slice();
+        myArray.push(item);
+        this.setState({data: myArray});
+    };
+
     render() {
         return (
             <div>
-                <h3>Array: {this.props.propArray}</h3>
-                <h3>Bool: {this.props.propBool ? 'True!' : 'False.'}</h3>
-                <h3>Func: {this.props.propFunc(3)}</h3>
-                <h3>Number: {this.props.propNumber}</h3>
-                <h3>String: {this.props.propString}</h3>
-                <h3>Object: {this.props.propObject.objectName1}</h3>
-                <h3>Object: {this.props.propObject.objectName2}</h3>
-                <h3>Object: {this.props.propObject.objectName3}</h3>
+                <button onClick={this.setStateHandler}>Set State</button>
+                <h4>State Array: {this.state.data}</h4>
             </div>
         )
     }
 }
-
-App.propTypes = {
-    propArray: React.PropTypes.array.isRequired,
-    propBool: React.PropTypes.bool.isRequired,
-    propFunc: React.PropTypes.func,
-    propNumber: React.PropTypes.number,
-    propString: React.PropTypes.string,
-    propObject: React.PropTypes.object
-};
-
-App.defaultProps = {
-    propArray: [1, 2, 3, 4, 5],
-    propBool: true,
-    propFunc: function(e) { return e },
-    propNumber: 1,
-    propString: "This is a string.",
-
-    propObject: {
-        objectName1: "objectValue1",
-        objectName2: "objectValue2",
-        objectName3: "objectValue3"
-    }
-};
 
 export default App;
