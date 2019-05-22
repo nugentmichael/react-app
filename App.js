af62fd27a83b59dcd18001681490e2ab5691c47a
nugentmichael <nugent.michael@gmail.com>
2019-05-22 13:37:30 -0400

    Demonstrating the component lifecycle methods.

--- App.js
+++ App.js
@@ -1,22 +1,63 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
 
 class App extends Component {
-    constructor() {
-        super();
-        this.findDomNodeHandler = this.findDomNodeHandler.bind(this);
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            data: 0
+        }
+
+        this.setNewNumber = this.setNewNumber.bind(this);
     };
 
-    findDomNodeHandler() {
-        const myDiv = document.getElementById('myDiv');
-        ReactDOM.findDOMNode(myDiv).style.color = 'blue';
+    setNewNumber() {
+        this.setState({data: this.state.data + 1});
     }
 
     render() {
         return (
             <div>
-                <button onClick={this.findDomNodeHandler}>Find DOM Node</button>
-                <div id="myDiv">Node</div>
+                <button onClick={this.setNewNumber}>Set New Number</button>
+                <Content myNumber={this.state.data} />
+            </div>
+        )
+    }
+}
+
+class Content extends Component {
+    componentWillMount() {
+        console.log('Component Will Mount');
+    }
+
+    componentDidMount() {
+        console.log('Component Did Mount');
+    }
+
+    componentWillReceiveProps(newProps) {
+        console.log('Component Will Receive Props');
+    }
+
+    shouldComponentUpdate(newProps, newState) {
+        return true;
+    }
+
+    componentWillUpdate(newProps, newState) {
+        console.log('Component Will Update');
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        console.log('Component Did Update');
+    }
+
+    componentWillUnmount() {
+        console.log('Component Will Unmount');
+    }
+
+    render() {
+        return (
+            <div>
+                <h3>{this.props.myNumber}</h3>
             </div>
         )
     }
--- main.js
+++ main.js
@@ -3,3 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App.js';
 
 ReactDOM.render(<App />, document.getElementById('app'));
+setTimeout(() => {
+    ReactDOM.unmountComponentAtNode(document.getElementById('app'));
+}, 10000);
