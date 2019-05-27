a9bc93dc184e19dc8d88e180b6b583b2625fc6ca
nugentmichael <nugent.michael@gmail.com>
2019-05-27 13:08:42 -0400

    Add ReactCSSTransitionGroup element to be used as a wrapper of the component to animate.

--- App.js
+++ App.js
@@ -1,35 +1,20 @@
 import React, { Component } from 'react';
+var ReactCSSTransitionGroup = require('react-addons-css-transition-group');
 
-class App extends Component {
+export default class App extends Component {
     render() {
         return (
             <div>
-                <Header />
-                <Content />
+                <ReactCSSTransitionGroup
+                    transitionName="example"
+                    transitionAppear={true}
+                    transitionAppearTimeout={500}
+                    transitionEnter={false}
+                    transitionLeave={false}
+                >
+                    <h1>My Element...</h1>
+                </ReactCSSTransitionGroup>
             </div>
         )
     }
 }
-
-class Header extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Header</h1>
-            </div>
-        )
-    }
-}
-
-class Content extends Component {
-    render() {
-        return (
-            <div>
-                <h2>Content</h2>
-                <p>This is the content.</p>
-            </div>
-        )
-    }
-}
-
-export default App;
