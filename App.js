bc0300060d4222dd42703756c1821154f85f8027
nugentmichael <nugent.michael@gmail.com>
2019-05-27 10:35:44 -0400

    Add defualt placeholder components.

--- App.js
+++ App.js
@@ -1,45 +1,49 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { Router, Route, Link, browserHistory, IndexRoute } from 'react-router';
 
 export default class App extends Component {
-    constructor() {
-        super();
-
-        this.state = {
-            data: [
-                {
-                    component: "First component...",
-                    id: 1
-                },
-                {
-                    component: "Second component...",
-                    id: 2
-                },
-                {
-                    component: "Third component...",
-                    id: 3
-                }
-            ]
-        };
-    }
-
     render() {
         return (
             <div>
-                <div>
-                    {this.state.data.map((dynamicComponent, i) => <Content key={i} componentData={dynamicComponent} />)}
-                </div>
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                </ul>
+
+                {this.props.children}
             </div>
         )
     }
 }
 
-class Content extends Component {
+class Home extends Component {
     render() {
         return (
             <div>
-                <div>{this.props.componentData.component}</div>
-                <div>{this.props.componentData.id}</div>
+                <h1>Home Page</h1>
             </div>
-        );
+        )
+    }
+}
+
+class About extends Component {
+    render() {
+        return (
+            <div>
+                <h1>About Page</h1>
+            </div>
+        )
+    }
+}
+
+class Contact extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Contact Page</h1>
+            </div>
+        )
     }
 }
