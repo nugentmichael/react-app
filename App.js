95f09891fae9ea25c6ad74bb8deef741e4f03688
nugentmichael <nugent.michael@gmail.com>
2019-05-27 13:19:32 -0400

    Add enter and leave animations when elements are added or removed from the list.

--- App.js
+++ App.js
@@ -2,9 +2,40 @@ import React, { Component } from 'react';
 var ReactCSSTransitionGroup = require('react-addons-css-transition-group');
 
 export default class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            items: ['Item 1...', 'Item 2...', 'Item 3...', 'Item 4...', 'Item 5...']
+        };
+
+        this.handleAdd = this.handleAdd.bind(this);
+    }
+
+    handleAdd() {
+        let newItems = this.state.items.concat([prompt('Create New Item')]);
+        this.setState({ items: newItems });
+    }
+
+    handleRemove(i) {
+        let newItems = this.state.items.slice();
+        newItems.splice(i, 1);
+        this.setState({ items: newItems });
+    }
+
     render() {
+        let items = this.state.items.map(function(item, i) {
+            return (
+                <div key={item} onClick={this.handleRemove.bind(this, i)}>
+                    {item}
+                </div>  
+            );
+        }.bind(this));
+
         return (
             <div>
+                <button onClick={this.handleAdd}>Add Item</button>
+                
                 <ReactCSSTransitionGroup
                     transitionName="example"
                     transitionAppear={true}
@@ -12,7 +43,7 @@ export default class App extends Component {
                     transitionEnter={false}
                     transitionLeave={false}
                 >
-                    <h1>My Element...</h1>
+                    {items}
                 </ReactCSSTransitionGroup>
             </div>
         )
