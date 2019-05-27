0241eab7c717f288e31ce14dc301c71baaedbbbb
nugentmichael <nugent.michael@gmail.com>
2019-05-27 10:51:04 -0400

    Add React Router component functionality.

--- main.js
+++ main.js
@@ -1,5 +1,60 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import App from './App.js';
+import { Router, Route, Link, browserHistory, IndexRoute } from 'react-router';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+export default class App extends Component {
+    render() {
+        return (
+            <div>
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                </ul>
+
+                {this.props.children}
+            </div>
+        )
+    }
+}
+
+export class Home extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Home Page</h1>
+            </div>
+        )
+    }
+}
+
+export class About extends Component {
+    render() {
+        return (
+            <div>
+                <h1>About Page</h1>
+            </div>
+        )
+    }
+}
+
+export class Contact extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Contact Page</h1>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render((
+    <Router history={browserHistory}>
+        <Route path="/" component={App}>
+            <IndexRoute component={Home} />
+            <Route path="home" component={Home} />
+            <Route path="about" component={About} />
+            <Route path="contact" component={Contact} />
+        </Route>
+    </Router>
+), document.getElementById('app'));
