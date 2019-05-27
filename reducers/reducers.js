b71bf8f8f4b10155b214d222eb3155331c8e9591
nugentmichael <nugent.michael@gmail.com>
2019-05-27 11:21:51 -0400

    Create new todos function to push new items to the list.

--- reducers/reducers.js
+++ reducers/reducers.js
@@ -12,3 +12,15 @@ function todo(state, action) {
             return state
     }
 }
+
+function todos(state = [], action) {
+    switch (action.type) {
+        case ADD_TODO:
+            return [
+                ...state,
+                todo(undefined, action)
+            ]
+        default:
+            return state
+    }
+}
